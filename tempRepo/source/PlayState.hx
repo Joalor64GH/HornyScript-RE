7ac5ce5ea0cadc925b5a7bf6eb644475cac331d0
Joalor64 <rhgarcon@gmail.com>
2023-05-19 09:17:56 -0400

    Update PlayState.hx

--- tempRepo/source/PlayState.hx
+++ tempRepo/source/PlayState.hx
@@ -3,6 +3,7 @@ package;
 import flixel.text.FlxText;
 import flixel.FlxState;
 import flixel.FlxG;
+import openfl.Lib;
 import horny.*;
 
 class PlayState extends FlxState
@@ -11,7 +12,16 @@ class PlayState extends FlxState
 	{
 		super.create();
 
-		var hscript:HornyScript = new HornyScript((Paths.script('script')));
+		try
+		{
+			var hscript:HornyScript = new HornyScript((Paths.script('script')));
+		}
+		catch (e)
+		{
+			var errText:FlxText = new FlxText(0, 0, 0, "Oops! There was an error with the script!" + "\n" + e.message, 64);
+        	errText.screenCenter();
+        	add(errText);
+		}
 
 		var coolText:FlxText = new FlxText(5, FlxG.height - 44, 0, "Hello World", 22);
 		coolText.scrollFactor.set();
